a4f5dd916e0cb11ef7363d2b2fc6bb9223eaa2ad
Victor <vikkindhart@gmail.com>
2015-01-12 19:44:30 +0100

    Fixed font rendering; several optimizatios

--- pyui-render.py
+++ pyui-render.py
@@ -10,8 +10,6 @@ template = """
 <style>
 div.View {
     position: fixed;
-    left: 0px;
-    top: 0px;
     border-radius: 10px;
 }
 
@@ -24,7 +22,6 @@ span.View-name-border {
     position: fixed;
     top: 30px;
     left: 0px;
-    width: 320;
     border-bottom: 1px solid #595959;
 }
 
@@ -46,50 +43,52 @@ div.TextField {
 
 template_View = """
 <div class="View" style="
-top: {0}px;
-left: {1}px;
-width: {2}px;
-height: {3}px;
-background-color: {4};
-border-color: {5};
-color: {6};
+top: {y}px;
+left: {x}px;
+width: {w}px;
+height: {h}px;
+background-color: {background_color};
+border-color: {border_color};
+color: {tint_color};
 ">
-<div class="View-name">{7}</div><br />
-<span class="View-name-border"></span>
+<div class="View-name">{name}</div><br />
+<span class="View-name-border" style="
+width: {w}px;
+"></span>
 </div>
 """
 
 template_Label = """
 <span class="Label" style="
-top: {0}px;
-left: {1}px;
-width: {2}px;
-height: {3}px;
-border-color: {4};
-color: {5};
-font-size: {6}px;
-font-family: {7};
-alignment: {8};
-">{9}</span>
+top: {y}px;
+left: {x}px;
+width: {w}px;
+height: {h}px;
+border-color: {border_color};
+color: {text_color};
+font-size: {font_size}px;
+font-family: {font_name};
+alignment: {alignment};
+">{text}</span>
 """
 
 template_TextField = """
 <div class="TextField" style="
-top: {0}px;
-left: {1}px;
+top: {y}px;
+left: {x}px;
 " ><input class="TextField" style="
-width: {2}px;
-height: {3}px;
-border-color: {4};
-color: {5};
-font-size: {6}px;
-font-family: {7};
-alignment: {8};
-border-width: {9}px;
+width: {w}px;
+height: {h}px;
+border-color: {border_color};
+color: {text_color};
+font-size: {font_size}px;
+font-family: {font_name};
+alignment: {alignment};
+border-width: {border_width}px;
 border-style: solid;
-border-radius: {10}px;
+border-radius: {corner_radius}px;
 padding: 0 5px 0 5px;
-" placeholder="{11}" value="{12}"></div>
+" placeholder="{placeholder}" value="{text}"></div>
 """
 
 pattern = "RGBA\(([\d.]+),([\d.]+),([\d.]+),([\d.]+)\)"
@@ -97,88 +96,79 @@ document = StringIO()
 
 pad_top = 30
 defaults = {
-    "font_name":   "<system>",
+    "font_name":   "Helvetica",
     "font_size":   17,
     "text":        "",
     "placeholder": ""
 }
 
-def convert(s):
-    r, g, b, a = re.findall(pattern, s)[0]
-    r = float(r) * 255
-    g = float(g) * 255
-    b = float(b) * 255
-    a = float(a)
+def convert(attrs, *labels):
+    for label in labels:
+        r, g, b, a = re.findall(pattern, attrs[label])[0]
+        r = float(r) * 255
+        g = float(g) * 255
+        b = float(b) * 255
+        a = float(a)
 
-    return "rgba(%d, %d, %d, %f)" % (r, g, b, a)
+        attrs[label] = "rgba(%d, %d, %d, %f)" % (r, g, b, a)
 
 def parse_frame(strframe):
     data = json.loads(strframe.replace("{", "[").replace("}", "]"))
 
-    frame = [data[0][0], data[0][1], data[1][0], data[1][1]]
+    frame = {
+        'x': data[0][0],
+        'y': data[0][1],
+        'w': data[1][0],
+        'h': data[1][1]
+    }
+    
     return frame
 
 # Rendering functions
 
-def render_View(name, (y, x, w, h), bg, bc, c):
-    bg = convert(bg)
-    bc = convert(bc)
-    c  = convert(c)
-    document.write(template_View.format(x, y, w, h, bg, bc, c, name))
+def render_View(**attrs):
+    convert(attrs, 'background_color', 'border_color', 'tint_color')
 
-def render_Label(label, (y, x, w, h), font_size, font_name, alignment, c, bc):
-    c  = convert(c)
-    bc = convert(bc)
-    document.write(template_Label.format(pad_top + x, y, w, h,
-        bc, c, font_size, font_name, alignment, label))
+    document.write(template_View.format(**attrs))
 
-def render_TextField(default, placeholder, (y, x, w, h),
-                     font_size, font_name, alignment, bw, cr,
-                     bc, c):
-    bc = convert(bc)
-    c  = convert(c)
-    document.write(template_TextField.format(pad_top + x, y, w, h,
-        bc, c, font_size, font_name, alignment,
-        bw, cr, placeholder, default))
+def render_Label(**attrs):
+    convert(attrs, 'text_color', 'border_color')
+    attrs['y'] += pad_top
+
+    document.write(template_Label.format(**attrs))
+
+def render_TextField(**attrs):
+    convert(attrs, 'border_color', 'text_color')
+    attrs['y'] += pad_top
+
+    document.write(template_TextField.format(**attrs))
 
 
 def render(json):
     view = json[0] # This renderer supports just one view for now
     attrs = view["attributes"]
     frame = parse_frame(view["frame"])
-    render_View(
-        attrs["name"],
-        frame,
-        attrs["background_color"],
-        attrs["border_color"],
-        attrs["tint_color"]
-    )
-    for element in view["nodes"]:
-        frame = parse_frame(element["frame"])
-        attrs = element["attributes"]
+    for (key, value) in frame.items():
+        attrs[key] = value
 
-        for key, value in defaults.items():
+    render_View(**attrs)
+
+    for element in view["nodes"]:
+        attrs = element["attributes"]
+        frame = parse_frame(element["frame"])
+        for (key, value) in frame.items():
+            attrs[key] = value
+
+        for (key, value) in defaults.items():
             if not key in attrs.keys():
                 attrs[key] = value
 
         if element["class"] == "Label":
-            render_Label(
-                attrs["text"],
-                frame,
-                attrs["font_size"] - 3, attrs["font_name"],
-                attrs["alignment"], attrs["text_color"],
-                attrs["border_color"]
-            )
+            render_Label(**attrs)
+
         elif element["class"] == "TextField":
-            render_TextField(
-                attrs["text"],
-                attrs["placeholder"],
-                frame,
-                attrs["font_size"] - 3, attrs["font_name"],
-                attrs["alignment"], attrs["border_width"],
-                attrs["corner_radius"], attrs["border_color"],
-                attrs["text_color"]
-            )
+            render_TextField(**attrs)
+
         else:
             print("Unknown (unsupported?) element: `%s'" % element["class"])
 
